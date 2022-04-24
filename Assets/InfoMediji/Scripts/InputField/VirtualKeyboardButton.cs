c27be9e0d1ad50f32d27e517d12ca93d37e378d0
nchernyadev <nikita.chernyadev@gmail.com>
2022-04-24 15:50:29 +0300

    Add VirtualKeyboardButton.cs class to implement button holding and automate event subscription

---
+++ Assets/InfoMediji/Scripts/InputField/VirtualKeyboardButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace InfoMediji.InputField
+{
+    [RequireComponent(typeof(Button))]
+    public class VirtualKeyboardButton : MonoBehaviour
+    {
+        [SerializeField]
+        private VirtualKeyboard keyboard;
+        [SerializeField]
+        private KeyCode keyCode;
+
+        private EventTrigger _eventTrigger;
+
+        private void Awake()
+        {
+            _eventTrigger = gameObject.AddComponent<EventTrigger>();
+
+            var down = new EventTrigger.Entry {eventID = EventTriggerType.PointerDown};
+            down.callback.AddListener(_ => keyboard.KeyDown(keyCode));
+
+            var up = new EventTrigger.Entry {eventID = EventTriggerType.PointerUp};
+            up.callback.AddListener(_ => keyboard.KeyUp(keyCode));
+
+            _eventTrigger.triggers.Add(down);
+            _eventTrigger.triggers.Add(up);
+        }
+    }
+}
